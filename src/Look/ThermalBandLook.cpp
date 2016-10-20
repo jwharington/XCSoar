1b8ae33075788e1dbdb631ff68d0accf970c865e
John Wharington <jwharington@gmail.com>
2016-10-20 20:40:53 +1100

    Look: optimise colors for dithered displays

--- src/Look/ThermalBandLook.cpp
+++ src/Look/ThermalBandLook.cpp
@@ -23,7 +23,8 @@ Copyright_License {
 
 #include "ThermalBandLook.hpp"
 #include "Screen/Layout.hpp"
-#include "Look/Colors.hpp"
+#include "Colors.hpp"
+#include "Asset.hpp"
 
 void
 ThermalBandLook::Initialise(bool _inverse, Color sky_color)
@@ -32,11 +33,20 @@ ThermalBandLook::Initialise(bool _inverse, Color sky_color)
 
   inverse = _inverse;
 
-  brush_active.Create(ColorWithAlpha(sky_color, alpha));
-  brush_inactive.Create(ColorWithAlpha(DarkColor(sky_color), alpha/2));
+  if (IsDithered()) {
+    brush_active.Create(ColorWithAlpha(COLOR_BLACK, alpha));
+    brush_inactive.Create(ColorWithAlpha(COLOR_GRAY, alpha/2));
 
-  pen_active.Create(Layout::ScalePenWidth(1), DarkColor(sky_color));
-  pen_inactive.Create(Layout::ScalePenWidth(1), sky_color);
+    pen_active.Create(Layout::ScalePenWidth(1), DarkColor(COLOR_GRAY));
+    pen_inactive.Create(Layout::ScalePenWidth(1), COLOR_BLACK);
+
+  } else {
+    brush_active.Create(ColorWithAlpha(sky_color, alpha));
+    brush_inactive.Create(ColorWithAlpha(DarkColor(sky_color), alpha/2));
+
+    pen_active.Create(Layout::ScalePenWidth(1), DarkColor(sky_color));
+    pen_inactive.Create(Layout::ScalePenWidth(1), sky_color);
+  }
 
   white_pen.Create(Layout::ScalePenWidth(2), COLOR_WHITE);
   black_pen.Create(Layout::ScalePenWidth(2), COLOR_BLACK);
