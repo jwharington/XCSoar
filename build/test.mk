97c73ca3162726757655c6467633dbd8162978ed
John Wharington <jwharington@gmail.com>
2023-08-10 02:36:47 +1000

    RunTask: extend to calculate off-track cruise

--- build/test.mk
+++ build/test.mk
@@ -1508,6 +1508,8 @@ RUN_TASK_SOURCES = \
 	$(SRC)/RadioFrequency.cpp \
 	$(SRC)/TransponderCode.cpp \
 	$(SRC)/Engine/Util/Gradient.cpp \
+	$(SRC)/Computer/CirclingComputer.cpp \
+	$(TEST_SRC_DIR)/FlightPhaseDetector.cpp \
 	$(DEBUG_REPLAY_SOURCES) \
 	$(TEST_SRC_DIR)/FakeTerrain.cpp \
 	$(TEST_SRC_DIR)/RunTask.cpp
