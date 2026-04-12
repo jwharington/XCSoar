be60d74a09adc865ae62a2ff3dc7d73b817c464d
John Wharington <jwharington@gmail.com>
2026-04-12 19:49:29 +1000

    tuned parameters

--- test/src/MultiAircraft/CovarianceTuner.hpp
+++ test/src/MultiAircraft/CovarianceTuner.hpp
@@ -18,21 +18,21 @@ namespace MultiAircraft
         bool enabled = false;
         std::string output_path = "ukf_covariance_tuned.json";
         std::size_t min_samples = 50;
-        double blend = 0.5;
+        double blend = 0.7;
         double min_diagonal = 1e-4;
-        std::size_t max_flights = 0;
-        std::size_t max_points_per_flight = 1500;
-        std::size_t max_failures_per_flight = 1500;
-        std::size_t max_consecutive_failures = 250;
-        std::size_t max_restarts_per_flight = 12;
-        std::size_t bootstrap_iterations = 200;
-        double bootstrap_fraction = 0.35;
+        std::size_t max_flights = 48;
+        std::size_t max_points_per_flight = 200;
+        std::size_t max_failures_per_flight = 120;
+        std::size_t max_consecutive_failures = 30;
+        std::size_t max_restarts_per_flight = 4;
+        std::size_t bootstrap_iterations = 32;
+        double bootstrap_fraction = 0.5;
         unsigned random_seed = 1337;
 
         bool convergence_enabled = true;
-        std::size_t convergence_min_passes = 2;
-        std::size_t convergence_max_passes = 8;
-        std::size_t convergence_flights_per_pass = 0;
+        std::size_t convergence_min_passes = 4;
+        std::size_t convergence_max_passes = 10;
+        std::size_t convergence_flights_per_pass = 48;
         double convergence_rel_tolerance = 0.02;
     };
 
